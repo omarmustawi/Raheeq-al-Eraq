0ea62a72f066d73453c6d8b167951ae35aeeb03d
omarmustawi <100845779+omarmustawi@users.noreply.github.com>
2024-02-28 22:11:59 +0300

    Update Team.tsx

--- src/Components/Team/Team.tsx
+++ src/Components/Team/Team.tsx
@@ -23,11 +23,13 @@ export default function Team() {
     >
       {/* section right */}
       <div className="h-fit grid-repeat justify-center items-center">
+      <Suspense fallback={<Skeleton style={"img-current"} />}>
         <Image
           src={images[currentImageIndex].src}
           alt="raheek-iraq-team"
           className="rounded-full border-4 border-yel img-current w-full h-full "
         />
+        </Suspense>
         {images.map((img, index) => (
           <Suspense key={index} fallback={<Skeleton style={img.style} />}>
             <Image
