6460924250c60efc22ec89281c5d66b5caded883
omarmustawi <100845779+omarmustawi@users.noreply.github.com>
2024-02-29 18:05:26 +0300

    Update HomePage.tsx

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -136,7 +136,7 @@ const HomePage = () => {
               className="w-ful"
               ref={carouselRef}
             >
-              {carouselItems.map((item, index) => (
+              {images.map((item, index) => (
                 <div className={`container-img`} key={index}>
                   <Image
                     className={`${
